1bb1d2e13d05e2b4b08ac238dc05aece76bab28c
Mateusz Haadyj <halas6@o2.pl>
2017-01-16 17:08:38 +0100

    wyglad + strona uzytkownika

---
+++ Magazyn3/App_Data/Controllers/UzytkownikTowaryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Magazyn3.Models;
+
+namespace Magazyn3.App_Data.Controllers
+{
+    [Authorize(Roles = "Uzytkownik")]
+    public class UzytkownikTowaryController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: UzytkownikTowary
+        public ActionResult Index()
+        {
+            return View(db.Towary.ToList());
+        }
+
+        // GET: UzytkownikTowary/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Towary towary = db.Towary.Find(id);
+            if (towary == null)
+            {
+                return HttpNotFound();
+            }
+            return View(towary);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
